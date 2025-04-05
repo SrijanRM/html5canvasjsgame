d8a72be850f85aec839b76e8966b75151d9a822d
unknown <srijancodex@gmail.com>
2025-04-05 20:41:46 +0530

    create enemy

--- index.js
+++ index.js
@@ -42,19 +42,74 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue')
 
 const projectiles = [];
+const ememies = [];
+
+function spawnEnemy() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4;
+
+        let x
+        let y
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+        const color = 'green';
+        const angle = Math.atan2(
+            canvas.height / 2 - y,
+            canvas.width / 2 - x
+        )
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        ememies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
+}
+
 
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0,0,canvas.width,canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.forEach(projectile => {
         projectile.update()
+    })
+    ememies.forEach((enemy) => {
+        enemy.update()
     })
 }
 
@@ -71,3 +126,4 @@ addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemy()
