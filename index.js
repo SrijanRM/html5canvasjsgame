dcae4b8eb4e6a5f6f2ee021c74d3c69403db1121
unknown <srijancodex@gmail.com>
2025-04-05 17:18:41 +0530

    projectile coordiante

--- index.js
+++ index.js
@@ -14,14 +14,56 @@ class Player {
     draw() {
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        c.fillStyle=this.color
+        c.fillStyle = this.color
         c.fill()
     }
 }
 
-const x = canvas.width /2 ;
-const y = canvas.height /2 ;
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue')
 player.draw()
-console.log(player);
+
+const projectiles = [];
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+}
+
+addEventListener('click', (event) => {
+
+    const angle = Math.atan2(event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+    )
+
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 }))
+})
+
+animate()
